8570dd954ee597d66f9af56bf5d660a18d77713c
Thomas <tienhau.it@gmail.com>
2024-09-09 10:17:46 +0700

    Module 8: Hot Module Reloading

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { SeedService } from './seed/seed.service';
 import { ConfigService } from '@nestjs/config';
 
+declare const module: any;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
@@ -18,5 +20,10 @@ async function bootstrap() {
     const configService = app.get(ConfigService);
 
     await app.listen(configService.get<number>('port'));
+
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
 }
 bootstrap();
